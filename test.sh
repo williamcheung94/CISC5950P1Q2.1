daeb7d94b6b2d47110c6cb822705cbb94af2de4b
williamcheung94 <61169234+williamcheung94@users.noreply.github.com>
2020-03-29 22:12:00 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1.1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1.1/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /project1.1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/pvqr-7yc4.csv /project1.1/input/
+./start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /CISC5950P1Q2.1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /CISC5950P1Q2.1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /CISC5950P1Q2.1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /CISC5950P1Q2.1/shot_logs.csv /CISC5950P1Q2.1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file ../../mapreduce-test-python/project1.1/mapper.py -mapper ../../mapreduce-test-python/project1.1/mapper.py \
--file ../../mapreduce-test-python/project1.1/reducer.py -reducer ../../mapreduce-test-python/project1.1/reducer.py \
--input /project1.1/input/* -output /project1.1/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /project1.1/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1.1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1.1/output/
-../../stop.sh
+-file /CISC5950P1Q2.1/mapper.py -mapper /CISC5950P1Q2.1/mapper.py \
+-file /CISC5950P1Q2.1/reducer.py -reducer /CISC5950P1Q2.1/reducer.py \
+-input /project1.1/input/* -output /CISC5950P1Q2.1/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /CISC5950P1Q2.1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /CISC5950P1Q2.1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /CISC5950P1Q2.1/output/
+./stop.sh
